448a65a66c4a15f02c6f8c4861d4b4548aa1e024
Oyekola Toheeb <toheeb.olawale.to23@gmail.com>
2023-05-18 17:33:38 +0100

    tuple conquered

---
+++ tuple/for_loop.py
@@ -0,0 +1,26 @@
+# list = []
+# dict = {}
+# tuple = ()
+
+# tuple are immutable
+# tuple are faster than list
+# tuple are used as keys in dictionaries
+
+numbers = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+print(type(numbers))
+
+for i in numbers:
+    print(i)
+
+months = ("Jan", "Feb", "Mar", "Apr", "May", "Jun",
+          "Jul", "Aug", "Sep", "Oct", "Nov", "Dec")
+
+for month in months:
+    print(month)
+
+locations = {
+    (35.6895, 39.6917): "Tokyo Office",
+    (40.7125, 74.6917): "New York Office",
+    (42.3423, 145.6917): "Canada Office",
+}
+# numbers[0] = 0
---
+++ tuple/methods.py
@@ -0,0 +1,4 @@
+numbers = (1, 2, 3, 3, 4, 5, 5, 5, (1, 2, 4, 5))
+print(numbers.count(5))
+print(numbers.index(3))
+print(numbers[4:])
